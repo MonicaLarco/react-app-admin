f3baa65084e2e6843ff862e6fd9c8c31727f090d
Monica Larco <mlarco94@gmail.com>
2023-03-19 18:45:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import SidebarMenu from "./scenes/global/Sidebar";
+import Dashboard  from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
+import Form from "./scenes/form";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 
 function App() {
+  const [theme, colorMode] = useMode();
+  // const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    <div className="app">
+      <SidebarMenu />
+      <main className="content">
+        <Topbar />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/team' element={<Team />} />
+          <Route path='/contacts' element={<Contacts />} />
+          <Route path='/form' element={<Form />} />
+        </Routes>
+      </main>
     </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
   );
 }
 
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+
+const Dashboard = () => {
+    return <div>
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="Dashboard" subtitle="Welcome to your Dashboard" />
+            </Box>
+        </Box>
+    </div>;
+}
+
+export default Dashboard;
